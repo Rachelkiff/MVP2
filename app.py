8904fc16ce704f1c39473b16cb1be51c7fcb1ccb
rachel <rachelkiffiak1@gmail.com>
2020-12-08 21:03:10 -0700

    Completed the user and login endpoints. Begain working on the recipe_post endpoint.

---
+++ app.py
@@ -0,0 +1,198 @@
+import mariadb
+from flask import Flask, request, Response
+import json
+import dbcreds
+from flask_cors import CORS
+import random
+import string
+
+app = Flask(__name__)
+CORS(app)
+def generateToken():
+  letters = string.ascii_letters
+  result_str = ''.join(random.choice(letters)for i in range (40))
+  return result_str
+
+
+#Login Endpoint, visitors should be able to login or create an account. 
+@app.route("/api/login", methods =["POST", "DELETE"])   
+def loginendpoint():
+    if request.method == "POST":
+      conn = None
+      cursor = None 
+      user_password = request.json.get("password")
+      user_email = request.json.get("email")
+      rows = None
+      user = None
+      try:
+        conn = mariadb.connect(user=dbcreds.user, password=dbcreds.password, host=dbcreds.host, port=dbcreds.port, database=dbcreds.database,)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM user WHERE password=? AND email=?", [user_password, user_email,])
+        user = cursor.fetchall()
+        print(user)
+        rows = cursor.rowcount
+        if (rows == 1):
+          cursor.execute("INSERT INTO session(user_id, login_token) VALUES (?,?)", [user[0], generateToken(),])
+          conn.commit()
+          rows = cursor.rowcount
+      except Exception as error:
+        print("Something went wrong: ")
+        print(error)   
+      finally:
+        if(cursor != None):
+         cursor.close()
+        if(conn != None):
+         conn.rollback()
+         conn.close()
+        if(rows == 1):
+          return Response("User login successfull!", mimetype="text/html", status=201)
+        else:
+          return Response("Information entered is not valid!", mimetype="text/html", status=500)
+    elif request.method == "DELETE":
+      conn = None
+      cursor = None 
+      user = None
+      user_loginToken = request.json.get("loginToken")
+      rows = None
+      try:
+        conn = mariadb.connect(user=dbcreds.user, password=dbcreds.password, host=dbcreds.host, port=dbcreds.port, database=dbcreds.database,)
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM session WHERE login_token=?", [user_loginToken,])
+        conn.commit()
+        rows = cursor.rowcount
+      except Exception as error:
+        print("Something went wrong: ")
+        print(error)     
+      finally:
+        if(cursor != None):
+         cursor.close()
+        if(conn != None):
+         conn.rollback()
+         conn.close()
+        if(rows == 1):
+          return Response("Delete Success!", mimetype="text/html", status=204)
+        else:
+          return Response("Login token invalid!", mimetype="text/html", status=500) 
+# User Endpoint          
+@app.route("/api/user", methods =["GET", "POST","DELETE"])
+def userendpoint():
+    if request.method == "GET":
+      conn = None
+      cursor = None
+      user = None
+      user = request.json.get("user")
+      rows = None
+      try:
+        conn = mariadb.connect(user=dbcreds.user, password=dbcreds.password, host=dbcreds.host, port=dbcreds.port, database=dbcreds.database,)
+        cursor = conn.cursor()
+        if user != "" and user != None:
+          cursor.execute("SELECT * FROM user WHERE id = ?", [id,])
+        else:
+          cursor.execute("SELECT * FROM user")
+        user = cursor.fetchall()
+      except Exception as error:
+        print("Something went wrong(: ")
+        print(error)   
+      finally:
+        if(cursor != None):
+         cursor.close()
+        if(conn != None):
+         conn.rollback()
+         conn.close()
+        if(user != None):
+            return Response(json.dumps(user, default=str), mimetype="application/json", status=200)
+        else:
+            return Response("User does not exist.", mimetype="text/html", status=500) 
+    elif request.method == "POST": 
+      conn = None
+      cursor = None 
+      user_username = request.json.get("username")
+      user_password = request.json.get("password")
+      user_email = request.json.get("email")
+      rows = None
+      try:
+        conn = mariadb.connect(user=dbcreds.user, password=dbcreds.password, host=dbcreds.host, port=dbcreds.port, database=dbcreds.database,)
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO user(username, password, email) VALUES (?, ?, ?)", [user_username, user_password, user_email,])
+        conn.commit()
+        rows = cursor.rowcount
+        if(rows == 1):
+          user = cursor.lastrowid
+          cursor.execute("INSERT INTO session(user_id, login_token) VALUES (?,?)", [user[0][0], generateToken()])
+          conn.commit()
+          rows = cursor.rowcount      
+      except Exception as error:
+        print("Something went wrong: ")
+        print(error)   
+      finally:
+        if(cursor != None):
+         cursor.close()
+        if(conn != None):
+         conn.rollback()
+         conn.close()
+        if(rows == 1):
+          return Response("User creation successfull!", mimetype="text/html", status=201)
+        else:
+          return Response("Username or email already exists!", mimetype="text/html", status=500)
+    elif request.method == "DELETE":
+      conn = None
+      cursor = None 
+      user_password = request.json.get("password")
+      user_loginToken = request.json.get("loginToken")
+      rows = None
+      try:
+        conn = mariadb.connect(user=dbcreds.user, password=dbcreds.password, host=dbcreds.host, port=dbcreds.port, database=dbcreds.database,)
+        cursor = conn.cursor()
+        cursor.execute("SELECT user_id FROM session WHERE login_token=?", [user_loginToken,])
+        user = cursor.fetchone()
+        print(user)
+        cursor.execute("DELETE FROM user WHERE id=? AND password=?", [user[0], user_password,])
+        conn.commit()
+        rows = cursor.rowcount
+      except Exception as error:
+        print("Something went wrong(This is LAZY!): ")
+        print(error)     
+      finally:
+        if(cursor != None):
+         cursor.close()
+        if(conn != None):
+         conn.rollback()
+         conn.close()
+        if(rows == 1):
+          return Response("Delete Success!", mimetype="text/html", status=204)
+        else:
+          return Response("Login token or password not valid!", mimetype="text/html", status=500)
+#Recipe Post endpoint
+@app.route("/api/recipe_post", methods =["GET", "POST","DELETE"])
+def recipepostendpoint():        
+    if request.method == "GET":
+      conn = None
+      cursor = None
+      user = None
+      user_id = request.args.get("user_id")
+      rows = None
+      try:
+        conn = mariadb.connect(user=dbcreds.user, password=dbcreds.password, host=dbcreds.host, port=dbcreds.port, database=dbcreds.database,)
+        cursor = conn.cursor()
+        if user != "" and user != None:
+          cursor.execute("SELECT * FROM user WHERE id = ?", [user_id,])
+        else:
+          cursor.execute("SELECT * FROM user")
+        user = cursor.fetchall()
+      except Exception as error:
+        print("Something went wrong: ")
+        print(error)   
+      finally:
+        if(cursor != None):
+         cursor.close()
+        if(conn != None):
+         conn.rollback()
+         conn.close()
+        if(user != None):
+            return Response(json.dumps(user, default=str), mimetype="application/json", status=200)
+        else:
+            return Response("User does not exist.", mimetype="text/html", status=500)
+
+
+
+
